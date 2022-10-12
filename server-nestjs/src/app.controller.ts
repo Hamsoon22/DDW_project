16ef9e5ce54aec8f7308a1ec25d989b309a8130e
David Zwart <davidzwa@gmail.com>
2022-10-12 18:23:48 +0200

    feat: add test client

--- server-nestjs/src/app.controller.ts
+++ server-nestjs/src/app.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Post, VERSION_NEUTRAL } from '@nestjs/common';
+import {
+  Body,
+  CACHE_MANAGER,
+  CacheInterceptor,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  UseInterceptors,
+  VERSION_NEUTRAL,
+} from '@nestjs/common';
 import { AppService } from '@/app.service';
 import { ApiTags } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { dreamStudioApiKeyToken } from '@/app.constants';
 import { generateAsync } from 'stability-client';
 import { RequestImageDto } from '@/dto/request-image.dto';
+import { Cache } from 'cache-manager';
+import { join, relative } from 'path';
 
 @ApiTags('Root')
 @Controller({
@@ -14,7 +26,10 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {}
+
+  readonly servePath = join(__dirname, '../../.out');
 
   @Get()
   getWelcome(): string {
@@ -23,11 +38,30 @@ export class AppController {
 
   @Post('dreamstudio-image')
   async generateImage(@Body() input: RequestImageDto) {
-    const apiKey = this.configService.get(dreamStudioApiKeyToken);
-    return await generateAsync({
-      prompt: input.prompt,
-      apiKey: apiKey,
-      ...input.options,
-    });
+
+    const image = await this.cacheManager.get(input.prompt);
+    if (!image) {
+      const apiKey = this.configService.get(dreamStudioApiKeyToken);
+      const result = (await generateAsync({
+        prompt: input.prompt,
+        apiKey: apiKey,
+        ...input.options,
+      })) as { images: any[] };
+
+      const imageRef = result.images[0];
+      const seed = imageRef.seed;
+      const fp = imageRef?.filePath;
+      const serveUrl = relative(this.servePath, fp);
+
+      const cacheEntry = {
+        prompt: input.prompt,
+        seed,
+        serveUrl,
+      };
+      await this.cacheManager.set(input.prompt, cacheEntry);
+      return cacheEntry;
+    } else {
+      return image;
+    }
   }
 }
