1162310bc2421a1a428b35053d34b6a3ee05c494
David Zwart <davidzwa@gmail.com>
2022-10-20 20:21:10 +0200

    some formatting

--- server-nestjs/src/app.controller.ts
+++ server-nestjs/src/app.controller.ts
@@ -1,21 +1,21 @@
-import { Body, Controller, Get, Post, VERSION_NEUTRAL } from '@nestjs/common';
-import { AppService } from '@/app.service';
-import { ApiTags } from '@nestjs/swagger';
-import { ConfigService } from '@nestjs/config';
-import { dreamStudioApiKeyToken } from '@/app.constants';
-import { generateAsync } from 'stability-client';
-import { RequestImageDto } from '@/dto/request-image.dto';
-import { shuffleArray } from '@/utils/random.utils';
+import { Body, Controller, Get, Post, VERSION_NEUTRAL } from "@nestjs/common";
+import { AppService } from "@/app.service";
+import { ApiTags } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
+import { dreamStudioApiKeyToken } from "@/app.constants";
+import { generateAsync } from "stability-client";
+import { RequestImageDto } from "@/dto/request-image.dto";
 
-@ApiTags('Root')
+@ApiTags("Root")
 @Controller({
-  version: VERSION_NEUTRAL,
+  version: VERSION_NEUTRAL
 })
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly configService: ConfigService,
-  ) {}
+    private readonly configService: ConfigService
+  ) {
+  }
 
   @Get()
   async getImages() {
@@ -24,13 +24,13 @@ export class AppController {
     });
   }
 
-  @Post('dreamstudio-image')
+  @Post("dreamstudio-image")
   async generateImage(@Body() input: RequestImageDto) {
     const apiKey = this.configService.get(dreamStudioApiKeyToken);
     const result = (await generateAsync({
       prompt: input.prompt,
       apiKey: apiKey,
-      ...input.options,
+      ...input.options
     })) as { images: any[] };
 
     const imageRef = result.images[0];
