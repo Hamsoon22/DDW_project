ab30992489fb4986e425526366c0fc76bcf9b159
hamsoon22 <dpdms040@gmail.com>
2022-10-20 22:45:19 +0200

    feat: footer almost complete

---
+++ client/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.scss";
+
+function getYear() {
+    return new Date().getFullYear();
+  }
+
+export function Footer() {
+    return (
+      <div className="footer">
+        <div className="footer-text-style"> &copy;{getYear()} Powered by DOT,  <a className="linkstyle" to={{href: "https://prod.y-box.online/" }}> Design Only Together</a>
+          <strong className="ps-5">
+          </strong>
+          , Designer : Yeun Kim | Developer : David Zwart
+          </div>
+      </div>
+    );
+  };
+  
+  export default Footer;
