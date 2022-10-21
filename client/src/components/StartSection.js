0965a882c79e944f6e19ce5f8ac2c3456f525f3f
hamsoon22 <dpdms040@gmail.com>
2022-10-21 15:06:22 +0200

    feat : title change

--- client/src/components/StartSection.js
+++ client/src/components/StartSection.js
@@ -5,7 +5,7 @@ import DButton from "./shared/DButton";
 const StartSection = () => {
   return (
     <section className="fullpage-center" id="start">
-      <h1>Future DDW 2022</h1>
+      <h1>How your past and future look like?</h1>
       <h2>
         We are going to ask questions about your past and future.
         <br />
