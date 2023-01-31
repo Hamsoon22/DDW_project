78f04b8749bb2f2b7b7d6eac5ef43d152fdb1ecf
hamsoon22 <dpdms040@gmail.com>
2023-01-31 16:26:40 +0100

    wip : add timer for showing next button

--- client/src/components/StartSection.js
+++ client/src/components/StartSection.js
@@ -1,26 +1,39 @@
 import { scrollToElem } from "../utilities";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DButton from "./shared/DButton";
 import Typewriter from 'typewriter-effect';
 
 const StartSection = () => {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
   const [hidden, setHidden] = useState(false);
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+  const [intervalId, setIntervalId] = useState(0);
 
-  // function clickStartEvent (){
-  //   setVisible(true);
-  //   // setHidden(true);
-  // }
+  const timeOutReset = () => {
+    const newIntervalId = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+      if (count === 3) {
+        console.log('stop!')
+        setCount(0);
+        setIntervalId();
+      }
+    }, 1000);
+    setIntervalId(newIntervalId);
+  }
 
   return (
     <section className="fullpage-center" id="start">
-      {!hidden && <button onClick={() => { setVisible(true); setHidden(true) }}>
+      {!hidden && <button onClick={() => {
+        setVisible(true);
+        setHidden(true);
+        timeOutReset();
+      }}>
         Click to Start
       </button>
       }
-
+      <h1>The component has been rendered for {count} seconds</h1>
       {visible &&
         <h1>
           <Typewriter
