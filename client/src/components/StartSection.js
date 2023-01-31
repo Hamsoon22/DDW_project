2fcaa2ffdb687d2a8017ed03b6b39e6f4c069498
hamsoon22 <dpdms040@gmail.com>
2023-01-31 14:48:22 +0100

    feat: add start button and typewriter

--- client/src/components/StartSection.js
+++ client/src/components/StartSection.js
@@ -3,32 +3,47 @@ import React from "react";
 import { useState } from "react";
 import DButton from "./shared/DButton";
 import Typewriter from 'typewriter-effect';
-import ReactCSSTransitionGroup from 'react-transition-group';
 
 const StartSection = () => {
+  const [visible, setVisible] = useState(false)
+  const [hidden, setHidden] = useState(false);
   const [show, setShow] = useState(false)
+
+  // function clickStartEvent (){
+  //   setVisible(true);
+  //   // setHidden(true);
+  // }
+
   return (
     <section className="fullpage-center" id="start">
-      <h1>
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .typeString("Welcome! ")
-              .pauseFor(1000)
-              .typeString("The Dream Sequencer collects anonymous data including text and images. ")
-              .pauseFor(1000)
-              .typeString("By clicking this button you consent to having your dreams saved in a database and posted on Instagram.")
-              .pauseFor(2000)
-              .deleteAll()
-              .start();
-          }}
-        />
-      </h1>
-      {show ?
-        <DButton text="click to continue" func={() =>
-          scrollToElem("introdcution")} /> : null
+      {!hidden && <button onClick={() => { setVisible(true); setHidden(true) }}>
+        Click to Start
+      </button>
       }
-    </section>
+
+      {visible &&
+        <h1>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Welcome! ")
+                .pauseFor(1000)
+                .typeString("The Dream Sequencer collects anonymous data including text and images. ")
+                .pauseFor(1000)
+                .typeString("By clicking this button you consent to having your dreams saved in a database and posted on Instagram.")
+                .pauseFor(2000)
+                .deleteAll()
+                .start();
+            }}
+          />
+        </h1>
+      }
+      {
+        show ?
+          <DButton text="click to continue" func={() =>
+            scrollToElem("introdcution")} /> : null
+      }
+    </section >
   );
 };
 
