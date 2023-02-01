d3ee4412d73386e74feb5091f68a07035c360da2
hamsoon22 <dpdms040@gmail.com>
2023-02-01 12:55:29 +0100

    fix : button appears in start section

--- client/src/components/StartSection.js
+++ client/src/components/StartSection.js
@@ -8,32 +8,17 @@ const StartSection = () => {
   const [visible, setVisible] = useState(false);
   const [hidden, setHidden] = useState(false);
   const [show, setShow] = useState(false);
-  const [count, setCount] = useState(0);
-  const [intervalId, setIntervalId] = useState(0);
-
-  const timeOutReset = () => {
-    const newIntervalId = setInterval(() => {
-      setCount(prevCount => prevCount + 1);
-      if (count === 3) {
-        console.log('stop!')
-        setCount(0);
-        setIntervalId();
-      }
-    }, 1000);
-    setIntervalId(newIntervalId);
-  }
-
+ 
   return (
     <section className="fullpage-center" id="start">
       {!hidden && <button onClick={() => {
         setVisible(true);
         setHidden(true);
-        timeOutReset();
+        setTimeout(() => { setShow(true) }, 30000)
       }}>
         Click to Start
       </button>
       }
-      <h1>The component has been rendered for {count} seconds</h1>
       {visible &&
         <h1>
           <Typewriter
