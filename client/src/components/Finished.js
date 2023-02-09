5375dcce754ab02f597f67989914b7a8daba5002
hamsoon22 <dpdms040@gmail.com>
2023-02-09 16:15:14 +0100

    [wip] add email form in finish

--- client/src/components/Finished.js
+++ client/src/components/Finished.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { scrollToElem } from "../utilities";
 import DButton from "./shared/DButton";
+import { useState } from "react";
 
 export function Finish() {
+  const [data, setData] = useState(null);
   return (
     <section className="fullpage-center" id="finish">
-      <h3>Thank you for submiting your answer.
-      <br />
-      Check others past and future images on the screen</h3>
-      <DButton text="start over" func={() => {
-      scrollToElem("start")
-      setTimeout(function() {
-       window.location.reload();
-      }, 1500);
-      }}
+      <h3>
+        <br />
+        If you would like to receive the images generated for you please leave your email address in the field below. We do not share nor keep the address.</h3>
+      <input
+        className="form_input"
+        type="text"
       />
-    </section>
-  )
+      <div className="btn-group" style={{ display: "flex" }}>
+        <DButton text="Yes please" func={() => {
+          scrollToElem("start")
+          setTimeout(function () {
+            window.location.reload();
+          }, 1500);
+        }}
+        />
+        <DButton text="No thanks" func={() => {
+          scrollToElem("start")
+          setTimeout(function () {
+            window.location.reload();
+          }, 1500);
+        }}
+        />
+      </div>
+      </section>
+      )
 }
