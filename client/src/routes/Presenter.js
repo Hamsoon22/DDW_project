f294c5847ada725bf4cf97028cebedf931dde7eb
hamsoon22 <dpdms040@gmail.com>
2023-02-10 16:28:08 +0100

    .

--- client/src/routes/Presenter.js
+++ client/src/routes/Presenter.js
@@ -18,97 +18,19 @@ export const Presenter = () => {
     await console.log(container);
   }, []);
 
-  const [images, setImages] = useState([]);
 
   const loadImages = useCallback(() => {
-    listImages().then((r) => {
-      const vals = r
-        .map((rval) => {
+    listImages().then((data) => {
           return {
-            url: `http://localhost:4000/${rval.serveUrl.replace("\\\\", "\\")}`,
-            timestamp: new Date(rval.lastModified).valueOf(),
+            url: `http://localhost:4000/$${data}}`,
           };
-        })
-        .sort((x, y) => x.timestamp - y.timestamp);
-
-      setImages(vals);
     });
-  }, [setImages]);
-
+  });
 
   return (
     <>
       <Background />
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 1,
-                duration: 0.2,
-              },
-            },
-          },
-
-          "particles": {
-            "number": {
-                "value": 8,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "line_linked": {
-                "enable": false
-            },
-            "move": {
-                "speed": 1,
-                "out_mode": "out"
-            },
-            "shape": {
-                "type": [
-                    "image",
-                    "circle"
-                ],
-                "image": [
-                 
-                ]
-            },
-            "color": {
-                "value": "#CCC"
-            },
-            "size": {
-                "value": 30,
-                "random": false,
-                "anim": {
-                    "enable": true,
-                    "speed": 4,
-                    "size_min": 10,
-                    "sync": false
-                }
-            }
-        },
-        "retina_detect": false
-        }}
-      />
+       {loadImages}
       <div className="stage" ref={null} />
     </>
   );
